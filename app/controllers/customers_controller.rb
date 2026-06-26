5262bbce686597e41ef6c362ed8ad55b699c7e64
Priyanshu8023 <priyanshukumar8023@gmail.com>
2026-06-26 07:22:06 +0000

    fix: customer destory endpoint

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -44,7 +44,9 @@ class CustomersController < ApplicationController
     logger.info("[LOG]: Customers#destroy customer_id=#{@customer.id} user_id=#{current_user&.id} request_id=#{request.request_id}")
     @customer.destroy
 
-    head :no_content
+    render json: {
+      message: "Customer Deleted Successfull"
+    },status: :ok
   end
 
   def summary
