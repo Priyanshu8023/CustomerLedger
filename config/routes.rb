4537aadc2bd8c8298f1e64abdaf206ea18ccb07e
Priyanshu Kumar <157643868+Priyanshu8023@users.noreply.github.com>
2026-06-30 14:49:24 +0530

    fix: remove a duplicate graphql route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -17,8 +16,8 @@ Rails.application.routes.draw do
 
   resources :customers, path: "customer", controller: "customers", only: %i[index show create update destroy]
 
-  resources :orders do 
-    collection do 
+  resources :orders do
+    collection do
       get :summary
     end
   end
